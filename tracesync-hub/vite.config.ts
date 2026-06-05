87a7eacd76253e31c572dd79a61d5fa283ddcab0
Diego Subires <diegosubires@gmail.com>
2026-06-05 05:05:57 +0200

    Corregir base: /tracesync/tracesync-hub/

--- tracesync-hub/vite.config.ts
+++ tracesync-hub/vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/tracesync/",
+  base: "/tracesync/tracesync-hub/",
   plugins: [react(), tailwindcss()],
 });
